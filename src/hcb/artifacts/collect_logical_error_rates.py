6921250562bc0ea7b030b62a97dad056ba4a8130
Craig Gidney <craiggidney@google.com>
2022-02-03 18:09:10 -0800

    Add command line args to collection

--- src/hcb/artifacts/collect_logical_error_rates.py
+++ src/hcb/artifacts/collect_logical_error_rates.py
@@ -1,54 +1,37 @@
+"""
+Collects Monte Carlo samples.
+
+Example usage:
+
+PYTHONPATH=src python src/hcb/artifacts/collect_logical_error_rates.py \
+    -case_error_rates 0.03 0.02 0.015 0.01 0.007 0.005 0.003 0.002 0.0015 0.001 0.0007 0.0005 0.0003 0.0002 0.0001 \
+    -case_observables H V \
+    -case_gate_sets SD6 SI1000 EM3_v2 \
+    -case_decoders internal internal_correlated \
+    -case_distances 3 5 7 9 11 \
+    -max_shots 1_000_000 \
+    -max_errors 1000 \
+    -merge_mode saturate \
+    -storage_location tmp.csv
+
+"""
+
+import argparse
 import math
 import pathlib
-from typing import Iterator, List
+from typing import Iterator, List, Sequence
 
 from hcb.codes.honeycomb.layout import HoneycombLayout
 from hcb.tools.analysis.collecting import collect_simulated_experiment_data, DecodingProblem
 
-OUT_DIR = pathlib.Path(__file__).parent.parent.parent.parent / "out"
 
-
-def iter_problems(decoders: List[str]) -> Iterator[DecodingProblem]:
-    error_rates = [
-        0.001,
-        0.0015,
-        0.002,
-        0.003,
-        0.005,
-        0.007,
-
-        0.03,
-        0.02,
-        0.015,
-        0.01,
-
-        0.0007,
-        0.0005,
-        0.0003,
-        0.0002,
-        0.0001
-    ]
-    gate_sets = [
-        'EM3_v2',
-        'SD6',
-        'SI1000',
-    ]
-    shearings = [
-        False,
-        # True,
-    ]
-    observables = [
-        'H',
-        'V',
-        # 'EPR',
-    ]
-    distances = [
-        3,
-        5,
-        7,
-        9,
-        11,
-    ]
+def iter_problems(*,
+                  decoders: List[str],
+                  error_rates: Sequence[float],
+                  shearings: Sequence[bool],
+                  gate_sets: Sequence[str],
+                  distances: Sequence[int],
+                  observables: Sequence[str]) -> Iterator[DecodingProblem]:
     for p in error_rates:
         for d in distances:
             for sheared in shearings:
@@ -71,17 +54,84 @@ def iter_problems(decoders: List[str]) -> Iterator[DecodingProblem]:
 
 
 def main():
-    data_file = OUT_DIR / 'data_fourth.csv'
+    parser = argparse.ArgumentParser(description='Collect Monte Carlo samples.')
+    parser.add_argument('-max_shots',
+                        type=int,
+                        required=True,
+                        help='Stops sampling a case if this many shots are acquired.')
+    parser.add_argument('-max_errors',
+                        type=int,
+                        required=True,
+                        help='Stops sampling a case if this many errors have been seen.')
+    parser.add_argument('-start_batch_size',
+                        type=int,
+                        default=10**2,
+                        help='Initial number of samples to batch together into one decoding call (this will be increased exponentially each call until hitting max_batch_size).')
+    parser.add_argument('-max_batch_size',
+                        type=int,
+                        default=10**5,
+                        help='Maximum number of samples to batch together into one decoding call.')
+    parser.add_argument('-storage_location',
+                        type=str,
+                        default='',
+                        help='The file to store results in (or read previous results from).')
+    parser.add_argument('-merge_mode',
+                        choices=['saturate', 'replace', 'append'],
+                        default='saturate',
+                        help='How to combine new results with old results.')
+    parser.add_argument('-threads',
+                        type=int,
+                        default=1,
+                        help='Number of threads to use.')
+
+    parser.add_argument('-case_error_rates',
+                        type=float,
+                        nargs='+',
+                        required=True,
+                        help='Physical error rates to sample from.')
+    parser.add_argument('-case_observables',
+                        choices=['H', 'V', 'EPR'],
+                        nargs='+',
+                        required=True,
+                        help='Which observable to intialize and protect and measure.')
+    parser.add_argument('-case_gate_sets',
+                        choices=['SD6', 'SI1000', 'EM3_v2'],
+                        nargs='+',
+                        required=True,
+                        help='Noisy gate sets to sample.')
+    parser.add_argument('-case_distances',
+                        type=int,
+                        nargs='+',
+                        required=True,
+                        help='Code distances to sample.')
+    parser.add_argument('-case_decoders',
+                        choices=['internal', 'internal_correlated', 'pymatching'],
+                        nargs='+',
+                        required=True,
+                        help='How to combine new results with old results.')
+    parser.add_argument('-case_sheared',
+                        type=bool,
+                        default=False,
+                        help='Uses sheared layouts.')
+
+    args = parser.parse_args()
 
     collect_simulated_experiment_data(
-        iter_problems(decoders=["internal", "internal_correlated"]),
-        out_path=data_file,
-        merge_mode="saturate",
-        start_batch_size=10**2,
-        max_batch_size=10**5,
-        max_shots=1_000_000,
-        max_errors=1000,
-        num_threads=6,
+        iter_problems(
+            decoders=args.case_decoders,
+            error_rates=args.case_error_rates,
+            distances=args.case_distances,
+            observables=args.case_observables,
+            shearings=[args.case_sheared],
+            gate_sets=args.case_gate_sets,
+        ),
+        out_path=args.storage_location or None,
+        merge_mode=args.merge_mode,
+        start_batch_size=args.start_batch_size,
+        max_batch_size=args.max_batch_size,
+        max_shots=args.max_shots,
+        max_errors=args.max_errors,
+        num_threads=args.threads,
     )
 
 
--- src/hcb/artifacts/make_threshold_plots.py
+++ src/hcb/artifacts/make_threshold_plots.py
@@ -63,16 +63,17 @@ def make_threshold_plots(
         groups: Dict[str, List[DesiredLineFit]]):
     data = project_intersection_of_both_observables(data)
     grouped = data.grouped_by(lambda e: (e.circuit_style, e.decoder))
-    widths = sorted({desc.data_width for desc in data.data.keys()}, reverse=True)
-    width_style = {
+    sizes = sorted({(desc.data_width, desc.data_height, desc.rounds)
+                    for desc in data.data.keys()}, reverse=True)
+    size_styles = {
         noise: (COLORS[k], MARKERS[k])
-        for k, noise in enumerate(widths)
+        for k, noise in enumerate(sizes)
     }
 
     fig, axs = plt.subplots(2, len(groups) + 1)
     for k, (name, gs) in enumerate(groups.items()):
         for y, g in enumerate(gs):
-            fill_in_threshold_plot(grouped, g, axs[y, k], width_style)
+            fill_in_threshold_plot(grouped, g, axs[y, k], size_styles)
             if k == 0:
                 axs[y, 0].set_ylabel(f"{g.legend_caption}\nLogical Error Rate")
                 axs[y, 0].yaxis.label.set_fontsize(14)
@@ -81,7 +82,15 @@ def make_threshold_plots(
         axs[-1, k].xaxis.label.set_fontsize(14)
 
     for y in [0, 1]:
-        axs[y, -1].legend(*axs[y, 0].get_legend_handles_labels())
+        labels = []
+        handles = []
+        for x in range(3):
+            a, b = axs[y, x].get_legend_handles_labels()
+            for k in range(len(a)):
+                if b[k] not in handles:
+                    labels.append(a[k])
+                    handles.append(b[k])
+        axs[y, -1].legend(labels, handles)
         axs[y, -1].get_xaxis().set_ticks([])
         axs[y, -1].get_yaxis().set_ticks([])
         axs[y, -1].spines['top'].set_visible(False)
@@ -95,10 +104,10 @@ def fill_in_threshold_plot(
     grouped_projected_data: Dict[Tuple[str, str], MultiStats],
     group: DesiredLineFit,
     ax: plt.Axes,
-    width_style: Dict[float, Tuple[Any, Any]],
+    size_styles: Dict[Tuple[float, float, float], Tuple[Any, Any]],
 ):
     stats = grouped_projected_data.get((group.filter_circuit_style, group.filter_decoder), MultiStats({}))
-    for data_width, noise_stats in stats.grouped_by(lambda e: e.data_width).items():
+    for size_key, noise_stats in stats.grouped_by(lambda e: (e.data_width, e.data_height, e.rounds)).items():
         xs = []
         ys = []
         ys_low = []
@@ -111,9 +120,8 @@ def fill_in_threshold_plot(
             low, high = case_stats.likely_error_rate_bounds(desired_ratio_vs_max_likelihood=1e-3)
             ys_low.append(low)
             ys_high.append(high)
-        color, marker = width_style[data_width]
-        rep = next(iter(noise_stats.data))
-        ax.plot(xs, ys, marker=marker, label=f'{rep.data_width}x{rep.data_height} rounds={rep.rounds}', color=color, zorder=10)
+        color, marker = size_styles[size_key]
+        ax.plot(xs, ys, marker=marker, label=f'{size_key[0]}x{size_key[1]} ({size_key[2]} rounds)', color=color, zorder=10)
         ax.fill_between(xs, ys_low, ys_high, alpha=0.3, color=color)
 
     ax.set_xlim(1e-4, 1e-1)
